3f8360eeef3c1530013795e5b2059ff4809c7c26
sharunspi <sharunkk1997@gmail.com>
2021-08-15 11:53:04 +0530

    added example

---
+++ example/components/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+
+// Attach to window. Can be useful to debug
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <main>
+      
+      </main>
+    );
+  }
+}
+
+export { App };
