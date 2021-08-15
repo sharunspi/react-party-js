a20244d56d05fa0db7cc576a83ff452cda847b6b
sharunspi <sharunkk1997@gmail.com>
2021-08-15 12:24:43 +0530

    added example

--- example/components/App.tsx
+++ example/components/App.tsx
@@ -1,14 +1,33 @@
 import * as React from 'react';
+import party from 'party-js'
 
 
 // Attach to window. Can be useful to debug
 
 class App extends React.Component {
-
+    private myRef;
+        constructor(props){
+            super(props);
+            this.myRef = React.createRef();
+        }
   render() {
+     
+    const showCelebration =() =>{
+            console.log(this.myRef.current)
+            party.confetti(this.myRef.current,{
+                count:party.variation.range(20,40),
+            })
+      }
     return (
       <main>
-     
+          <div>
+              <button onClick={()=>showCelebration()}>
+                    show celebration
+              </button>
+          </div>
+          <div ref={this.myRef}>
+
+          </div>
       </main>
     );
   }
