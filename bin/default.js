3287ef58fe1855b23e96567e6afd5d47c6fecfd1
sharunspi <sharunkk1997@gmail.com>
2021-10-03 10:05:31 +0530

    added default options

---
+++ bin/default.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _objectAssign = _interopRequireDefault(require("object-assign"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
---
+++ src/default.js
@@ -0,0 +1,14 @@
+import assign from 'object-assign';
+
+let defaults = {
+    template : 'Confetti',
+    Count: 10,
+    Size: 10,
+    Spread: 10,
+};
+
+function mergeOptions(options) {
+    defaults = assign(defaults, options);
+}
+
+export { defaults, mergeOptions };
